8fc49ee6583520ad2f0b6b4635412dc584b828e8
Jularin <58310353+Jularin@users.noreply.github.com>
2019-12-01 18:20:59 +0300

    Update soursecode.py
    Full working programm with solving euler problem №22

--- soursecode.py
+++ soursecode.py
@@ -1,35 +1,50 @@
-def parser(filename:str, separator:str, ending:str):
+def parser_in_file(filename:str, separator:str):
 	"""
 	Parsing text file(filename), with separator
-	stops by ending, like:
-	LENNIE,KIARA,JACALYN,CARLOTA,1
+	parse while have values,
+	 like:
+	LENNIE,KIARA,JACALYN,CARLOTA
 	separator = ","
-	ending = "1"
 	Return list with ['LENNIE', 'KIARA', 'JACALYN', 'CARLOTA']
 	RUS:
 	Сортирует значения в файле с именем filename и расширением .txt по разделителю separator
-	останавливается по значению ending
+	Сортирует пока есть значения
 	Пример текста:
-	LENNIE,KIARA,JACALYN,CARLOTA,1
+	LENNIE,KIARA,JACALYN,CARLOTA
 	separator = ","   #разделитель
-	ending = "1"      #значение по которому останавливается 
 	Возвращает список с ['LENNIE', 'KIARA', 'JACALYN', 'CARLOTA']
 	"""
-	list = []
-	sortedlist = []
-	file = open(filename)
-	for x in file:
-		list.append(x)
-	namesstr = list[0] #make str variable with names(делаем строку из сортируемых значений)
-	i = 0
-	while 1:
-		if namesstr[:i]== ending: #end if meet ending(выполнение программы останавливается если встречается значение ending)
-			break
-		else:
+	try:
+		List = []
+		sortedlist = []
+		file = open(filename)
+		for x in file:
+			List.append(x)
+		namesstr = List[0] #make str variable with names(делаем строку из сортируемых значений)
+		i = 0
+		while 1:
 			if namesstr[i]==separator:
 				sortedlist.append(namesstr[:i]) #select a word or value, cut and append to list(находим слово или значение, делаем срез и отпраляем его в список)
 				namesstr = namesstr[i+1:] #cutiing word or value from string with values(Вырезаем значение из строки)
 				i = 0
 			else:
 				i+=1
-	return sortedlist			
+	except Exception:
+		return sorted(sortedlist)
+
+
+def score(string:str):
+	alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+	summ = 0
+	for i in range(len(string)):
+		if string[i] in alphabet:
+			summ+= (alphabet.index(string[i]))+1
+	return summ
+
+
+def score_in_list(plase:int,List:list):
+	scorer = score(List[plase-1].lower())
+	return scorer*plase
+
+List = parser_in_file("names.txt",",")
+print(score_in_list(938,List))
